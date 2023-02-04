e1b5ca29666963ac537117e2e78e229ed0971fcb
mfahadarif85 <mfahadarif@gmail.com>
2023-02-04 23:29:50 +0500

    pricing comp left side

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,7 +1,23 @@
-import React from "react";
-
+import { Button, Box, Flex, Heading, Text } from "@chakra-ui/react";
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return (
+    <Box>
+      <Flex>
+        <Box bg='#f0eafb' p='60px' textAlign='center'>
+          <Text fontSize='24px' fontWeight='800'>
+            Premium PRO
+          </Text>
+          <Heading fontSize='60px' pt='16px'>
+            $329
+          </Heading>
+          <Text pt='8px'>billed just once</Text>
+          <Button colorScheme='purple' size='lg' w='282px' mt='24px'>
+            Get Started
+          </Button>
+        </Box>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Pricing;
